5a69f2814e5426dedd3563773e5b3dbf961137b4
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-05-25 23:29:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-//your JS code here. If required.
-document.addEventListener('DomContentLoaded', () => {
-	const codes = document.querySelectorAll('.code');
+document.addEventListener('DOMContentLoaded', () => {
+    const codes = document.querySelectorAll('.code');
+    
+    codes.forEach((code, index) => {
+        code.addEventListener('input', () => {
+            if (code.value.length === code.maxLength) {
+                if (index < codes.length - 1) {
+                    codes[index + 1].focus();
+                }
+            }
+        });
 
-	codes.forEach((code, index) => {
-		code.addEventListener('input', () => {
-			if (code.value.length === code.maxlength) {
-				if (index < code.length - 1) {
-					codes[index + 1].focus();
-				}
-			}
-		})	
-	code.addEventListener('keydown', (e) => {
-		if (e.key === 'Backspace' && code.value === '') {
-			if (index > 0) {
-				codes[index - 1].focus();
-			}
-		}
-	})
-  })
-	
-})
+        code.addEventListener('keydown', (e) => {
+            if (e.key === 'Backspace' && code.value === '') {
+                if (index > 0) {
+                    codes[index - 1].focus();
+                }
+            }
+        });
+    });
+});
